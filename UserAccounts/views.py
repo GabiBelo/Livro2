511312d84cd453483b9d2958bdae2c28656c3908
GabiBelo <gabi_bdb@hotmail.com>
2021-01-25 20:31:44 -0300

    .
    Signed-off-by: GabiBelo <gabi_bdb@hotmail.com>

--- UserAccounts/views.py
+++ UserAccounts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 
 from .models import Topic
-from .forms import TopicForm
+from .forms import TopicForm, EntryForm
 
 
 # Create your views here.
@@ -35,3 +35,18 @@ def new_topic(request):
             return redirect('User_Accounts:topics')  # 6
     context = {'form': form}  # 7
     return render(request, 'User_Accounts/new_topic.html', context)
+
+
+def new_entry(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)  # 1
+    if request.method != 'POST':  # 2
+        form = EntryForm()  # 3
+    else:
+        form = EntryForm(data=request.POST)  # 4
+        if form.is_valid():
+            new_entry = form.save(commit=False)  # 5
+            new_entry.topic = topic  # 6
+            new_entry.save()
+            return redirect('User_Accounts:topic', topic_id=topic_id)  # 7
+    context = {'topic': topic, 'form': form}
+    return render(request, 'User_Accounts/new_entry.html', context)
